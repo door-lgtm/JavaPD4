f35472fd9864b9b64b8dd8ed60f74c1339adbc72
door-lgtm <justinaron000@gmail.com>
2025-09-17 16:33:20 +0000

    c

---
+++ Java-4-Activity/Main.java
@@ -0,0 +1,57 @@
+class Main {
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+ void init(){
+// Define a variable for each situation in the challenges below. Assign a value to each variable.
+// Display the value of each variable to the console.
+
+/*  Challenge 1:    
+    1) student's age 
+    2) number of people that live with you
+    3) number of items in a cart 
+*/
+   int student_age = 17;
+   System.out.println(student_age);
+   System.out.println("student's age is"+ student_age);
+
+   int mumber 
+
+
+
+
+
+/*  Challenge 2:
+    1) temperature 
+    2) student's GPA
+    3) growth rate of your investment
+*/
+    int current_temp = 68;
+    System.out.println(current_temp);
+    System.out.println("the current temperature is" + current_temp);
+
+
+/*  Challenge 3:
+    1) Student's last name 
+    2) student's first initial
+    3) description of an item 
+*/
+    int student_first = J;
+    System.out.println(student_first);
+    System.out.println("Students first initial is " + student_first);
+
+
+/*  Challenge 4:
+    1) the status whether lights are on
+    2) the status whether the game is over
+    3) the status whether the battery is charging
+*/
+    int battery_charge = batt
+
+
+    // **************************************************
+    // **** Don't write any code below here.  ***********
+    // **************************************************
+  }
+}
